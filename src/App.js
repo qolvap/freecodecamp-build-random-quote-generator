a416182ca2ee30aa3ceac929abf80aab3704b179
qolvap <pgrebennikow@yahoo.pl>
2024-02-21 13:53:59 +0100

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({});
+
+  useEffect(() => {
+    getNewQuote();
+  }, []);
+
+  const getNewQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuoteInfo({
+          quote: data.content,
+          author: data.author,
+        });
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <div id='quote-box'>
+        <p id='text'>{quoteInfo.quote}</p>
+        <p id='author'>{quoteInfo.author}</p>
+        <button id='new-quote' onClick={getNewQuote}>
+          New Quote
+        </button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={'http://twitter.com/intent/tweet'}
+          target='_blank'
+          id='tweet-quote'
+          onClick={getNewQuote}
         >
-          Learn React
+          Tweet it
         </a>
-      </header>
+      </div>
     </div>
   );
 }
